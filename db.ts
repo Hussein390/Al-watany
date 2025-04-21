71e66d6c5c6f688fa26147527668519432561541
Your Name <hsssseeeen2002@gmail.com>
2025-04-21 08:51:03 +0300

    fixing prisma client

--- db.ts
+++ db.ts
@@ -2,7 +2,14 @@ import { PrismaClient } from "@prisma/client"
  
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
  
-export const db = globalForPrisma.prisma || new PrismaClient()
+export const db = globalForPrisma.prisma || new PrismaClient({
+    // required to avoid the 'prepared statement already exists' error
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  })
  
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
 
